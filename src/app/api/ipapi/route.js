605b3623bbb3ee9ca2a0603d6e242e3368c74710
Cho Hei Pei <z5296007@ad.unsw.edu.au>
2024-02-29 13:52:57 +0800

    Added vercel ip geolocation

--- src/app/api/ipapi/route.js
+++ src/app/api/ipapi/route.js
@@ -3,8 +3,17 @@
 import { NextResponse } from "next/server";
 import { revalidatePath } from "next/cache";
 
+export const runtime = "edge";
+
 export async function GET(request) {
   revalidatePath(request.url);
+
+  const clientcountry = request.headers.get("X-Vercel-IP-Country");
+
+  if (clientcountry) {
+    return NextResponse.json({ countryCode: clientcountry }, { status: 200 });
+  }
+
   const data = await fetch(`https://ipapi.co/json/`).then((res) => res.json(), {
     cache: "no-store",
   });
