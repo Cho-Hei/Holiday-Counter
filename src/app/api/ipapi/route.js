9e729bc8e3fec3f25274a518cdceb1f8d498760d
Cho Hei Pei <z5296007@ad.unsw.edu.au>
2025-01-01 12:44:02 +0800

    fix country fetching

--- src/app/api/ipapi/route.js
+++ src/app/api/ipapi/route.js
@@ -6,19 +6,19 @@ import { revalidatePath } from "next/cache";
 export const runtime = "edge";
 
 export async function GET(request) {
-  revalidatePath(request.url);
+    revalidatePath(request.url);
 
-  const clientcountry = request.headers.get("X-Vercel-IP-Country");
+    // const clientcountry = request.headers.get("X-Vercel-IP-Country");
 
-  if (clientcountry) {
-    return NextResponse.json({ countryCode: clientcountry }, { status: 200 });
-  }
+    // if (clientcountry) {
+    //   return NextResponse.json({ countryCode: clientcountry }, { status: 200 });
+    // }
 
-  const data = await fetch(`https://ipapi.co/json/`).then((res) => res.json(), {
-    cache: "no-store",
-  });
+    const data = await fetch(`https://ipapi.co/json/`).then((res) => res.json(), {
+        cache: "no-store",
+    });
 
-  return NextResponse.json(data, {
-    status: 200,
-  });
+    return NextResponse.json(data, {
+        status: 200,
+    });
 }
