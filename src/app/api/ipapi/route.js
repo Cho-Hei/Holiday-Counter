74ddb85db6132b345b12c1eec33ce27196ad86f7
Cho Hei Pei <z5296007@ad.unsw.edu.au>
2025-01-01 12:49:12 +0800

    use vercel request headers

--- src/app/api/ipapi/route.js
+++ src/app/api/ipapi/route.js
@@ -8,12 +8,18 @@ export const runtime = "edge";
 export async function GET(request) {
     revalidatePath(request.url);
 
-    // const clientcountry = request.headers.get("X-Vercel-IP-Country");
+    // Country Fetching (only for Vercel)
+    const clientcountry = request.headers.get("X-Vercel-IP-Country");
+    const timezone = request.headers.get("x-vercel-ip-timezone");
 
-    // if (clientcountry) {
-    //   return NextResponse.json({ countryCode: clientcountry }, { status: 200 });
-    // }
+    if (clientcountry) {
+        return NextResponse.json(
+            { countryCode: clientcountry, timezone: timezone },
+            { status: 200 }
+        );
+    }
 
+    // Country Fetching
     const data = await fetch(`https://ipapi.co/json/`).then((res) => res.json(), {
         cache: "no-store",
     });
